ca3454c83f6eca458c23023e6d8eaef3aeb24c63
Sufyan Liaquat <sufyanliaquat58@gmail.com>
2025-11-19 00:04:44 +0500

    updated home.jsx to lazy import all pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import { Navbar } from "../components/Navbar";
-import { HeroSection } from "../components/HeroSection";
-import { AboutSection } from "../components/AboutSection";
-import { SkillsSection } from "../components/SkillsSection";
-import { ExperienceSection } from "../components/ExperienceSection";
-import { ProjectsSection } from "../components/ProjectsSection";
-import { ContactSection } from "../components/ContactSection";
-import { Footer } from "../components/Footer";
+import { Suspense, lazy } from "react";
+const Navbar = lazy(() => import("../components/Navbar").then(module => ({ default: module.Navbar })));
+const HeroSection = lazy(() => import("../components/HeroSection").then(module => ({ default: module.HeroSection })));
+const AboutSection = lazy(() => import("../components/AboutSection").then(module => ({ default: module.AboutSection })));
+const SkillsSection = lazy(() => import("../components/SkillsSection").then(module => ({ default: module.SkillsSection })));
+const ExperienceSection = lazy(() => import("../components/ExperienceSection").then(module => ({ default: module.ExperienceSection })));
+const ProjectsSection = lazy(() => import("../components/ProjectsSection").then(module => ({ default: module.ProjectsSection })));
+const ContactSection = lazy(() => import("../components/ContactSection").then(module => ({ default: module.ContactSection })));
+const Footer = lazy(() => import("../components/Footer").then(module => ({ default: module.Footer })));
 
 export const Home = () => {
   return (
